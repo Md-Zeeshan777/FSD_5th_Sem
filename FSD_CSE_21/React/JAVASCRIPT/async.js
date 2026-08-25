c3bb7af1860ba6a9c5618d485d719ea751e1edc1
Md-Zeeshan777 <gtxhelacruse@gmail.com>
2026-08-25 09:38:03 +0530

    js

---
+++ FSD_CSE_21/React/JAVASCRIPT/async.js
@@ -0,0 +1,11 @@
+console.log("task one");
+console.log("task two");
+
+setTimeout(()=>{
+    console.log("task three");
+},1000);
+setTimeout(()=>{
+    console.log("task tree point five");
+},0);
+console.log("task four");
+console.log("task five");
