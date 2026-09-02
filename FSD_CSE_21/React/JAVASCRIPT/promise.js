f9495d9628cd0cf1457c63ca07b744be222c7154
Md-Zeeshan777 <gtxhelacruse@gmail.com>
2026-09-02 14:43:37 +0530

    js

---
+++ FSD_CSE_21/React/JAVASCRIPT/promise.js
@@ -0,0 +1,24 @@
+function f1(){
+    return new Promise((resolve, reject)=>{
+       setTimeout(()=>{
+        console.log("HI");
+        resolve();
+    }, 4000); 
+    })
+    
+}
+
+function f2(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        console.log("There");
+        resolve();
+    }, 1000); 
+    })
+    
+
+}
+f1().then(f2)
+    .catch((err)=>{
+        console.log("ERROR", err)
+    })
